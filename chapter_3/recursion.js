8790e6b5a224c4f2ab09d8ce23d8a8783d728c42
JordanCollier <jordanicollier@gmail.com>
2015-05-09 13:29:32 -0600

    solution for recursion challenge

---
+++ chapter_3/recursion.js
@@ -0,0 +1,21 @@
+function isEven(number) {
+  if (number < 0) {
+    number *= -1;
+  }
+  if (number === 0) {
+  	return true;
+  } else if (number === 1) {
+	return false;
+  } else {
+    return isEven(number - 2);
+  }
+}
+
+console.log(isEven(0));
+console.log(isEven(1));
+console.log(isEven(50));
+// → true
+console.log(isEven(75));
+// → false
+console.log(isEven(-1));
+// → ??
